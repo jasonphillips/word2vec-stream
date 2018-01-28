ba574aa505e014aaf3fe751f4f88617744ddcf6d
Jason <jphillips@ua.edu>
2018-01-28 10:01:34 -0600

    fixed possible early exit when blank space encountered at beginning of buffer sequence

--- index.js
+++ index.js
@@ -19,11 +19,54 @@ function streamBinaryModel(binpath) {
     instream.on('error', reject);
 
     instream.on('end', () => outstream.emit('end'));
+
+    const readNext = function() {
+      let wordBuffer = instream.read(MAX_STRING + size * 4);
+      let off = 0; // offset
+      if (wordBuffer===null) return;
+                    
+      let word = wordBuffer.toString().split(' ')[0];
+
+      if (!word || word.length===0) {
+        // we must have eaten a dead space at beginning
+        wordBuffer = wordBuffer.slice(1);
+        off += 1;
+      }
+
+      const values = new Float32Array(size);
+                  
+      off += word.length + 1;
+      for (let i = 0; i < size; i++) {
+        try {
+          values[i] = wordBuffer.readFloatLE(off);
+        } catch(e) { 
+          // our data must be at an end, return early
+          return;
+        }
+        off += 4;
+      }
+                
+      // normalize 
+      const len = Math.sqrt(values.reduce(function (added, val) { return added + val * val }, 0));
+      
+      for (let a = 0; a < size; a++) {
+        values[a] = values[a] / len;
+      }
+        
+      // we read too much, push remainder back onto stack
+      const actualLength = word.length + 1 + size * 4;
+      if (actualLength < wordBuffer.length) {
+          instream.unshift(wordBuffer.slice(actualLength));
+      }
+      
+      outstream.push({word: word.trim(), values: values});
+    }
     
     instream.on('readable', function() { 
       if (size) {
         // already read metadata; simply notify to resume
-        return outstream._read();
+        readNext();
+        return;
       }
 
       // read metadata once 
@@ -37,41 +80,7 @@ function streamBinaryModel(binpath) {
       
       if (pos < 50) instream.unshift(buffer.slice(pos));
             
-      outstream._read = function() {
-        const wordBuffer = instream.read(MAX_STRING + size * 4);
-        if (wordBuffer===null) return;
-                      
-        const word = wordBuffer.toString().split(' ')[0];
-        if (!word || word.length===0) return;
-
-        const values = new Float32Array(size);
-                    
-        let off = word.length + 1;
-        for (let i = 0; i < size; i++) {
-          try {
-            values[i] = wordBuffer.readFloatLE(off);
-          } catch(e) { 
-            // our data must be at an end, return early
-            return;
-          }
-          off += 4;
-        }
-                  
-        // normalize 
-        const len = Math.sqrt(values.reduce(function (added, val) { return added + val * val }, 0));
-        
-        for (let a = 0; a < size; a++) {
-          values[a] = values[a] / len;
-        }
-          
-        // we read too much, push remainder back onto stack
-        const actualLength = word.length + 1 + size * 4;
-        if (actualLength < wordBuffer.length) {
-            instream.unshift(wordBuffer.slice(actualLength));
-        }
-        
-        outstream.push({word: word.trim(), values: values});
-      }
+      outstream._read = readNext;
         
       resolve(outstream);
     });
